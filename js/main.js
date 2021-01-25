00a285888f165e5ca40f9ea6de852de68d9d9cef
Sergey <66194887+GMDdesign@users.noreply.github.com>
2021-01-25 22:34:40 +0700

    Beta version 1.1.1
    Рабочая бета-версия

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-function addition() {
-    var a = parseInt(document.getElementById('a').value);
-    var b = parseInt(document.getElementById('b').value);
-
-    if (isNaN(a)==true) a=0;
-    if (isNaN(b)==true) b=0;
-
-    var c = a + b;
-}
 function showModalWin() {
  
     var darkLayer = document.createElement('div');
@@ -14,6 +5,583 @@ function showModalWin() {
     document.body.appendChild(darkLayer);
 
     var modalWin = document.getElementById('popupWin');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+
+function showModalWin2() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin2');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+
+function showModalWin3() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin3');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+
+function showModalWin4() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin4');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+
+function showModalWin5() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin5');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+
+function showModalWin6() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin6');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+
+function showModalWin7() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin7');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+
+function showModalWin8() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin8');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin9() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin9');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin10() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin10');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin11() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin11');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin12() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin12');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin13() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin13');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin14() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin14');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin15() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin15');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin16() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin16');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin17() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin17');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin18() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin18');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin19() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin19');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin20() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin20');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin21() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin21');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin22() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin22');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin23() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin23');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin24() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin24');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin25() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin25');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin26() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin26');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin27() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin27');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin28() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin28');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin29() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin29');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin30() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin30');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin31() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin31');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin32() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin32');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin33() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin33');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin34() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin34');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin35() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin35');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin36() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin36');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin37() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin37');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin38() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin38');
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+function showModalWin39() {
+ 
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('popupWin39');
     modalWin.style.display = 'block';
 
     darkLayer.onclick = function () {
